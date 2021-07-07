bef1651b9505efdda81dfe191bc9c21ae266cf9d
Jian Ma <majian@Jians-Mac-mini.local>
2021-07-07 16:48:47 +0800

    first commit

---
+++ app.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (a *App) Initialize(user, password, dbname string) {
+
+}
+
+func (q *App) Run(addr string) {}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/majian2014/go-mux-rest
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+)
